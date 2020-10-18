7cfd25e772c4ee397b7ab440c3aef0a2d26af287
AndrewDunham <dunham.andrew2000@gmail.com>
2020-10-17 21:33:29 -0700

    Added json and allows for recipes to be added.

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-"""
-This is the script for querying the database.
-"""
+import json
+
+def submitRecipe(data, fileName="Recipes.json"):
+    with open(fileName, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def newRecipe():
+    with open('Recipes.json') as jsonFile:
+        data = json.load(jsonFile)
+
+        moreFood = data['Recipes']
+
+        #Get the last recipe's ID and add one
+        ###NEEDS A CONDITION IF THERE ISN'T ANY 0 element
+        RecipeID = (moreFood[-1]['RecipeID']) + 1
+
+        newRecipe = {
+            "RecipeID":RecipeID,
+            "RecipeName":"",
+            "Description":"",
+            "DateCreated":"",
+            "LastMade":"",
+            "Steps":[""],
+            "Details":{
+                "Prep":"",
+                "Cook":"",
+                "Total":"",
+                "Servings":""
+                },
+                "ImageURL":""
+            }
+
+        moreFood.append(newRecipe)
+    submitRecipe(data)
+
+
+
+newRecipe()
